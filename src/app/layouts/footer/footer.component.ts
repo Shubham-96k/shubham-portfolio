d8f06c599a29b5c402ccc296407bd7a01191efa1
Shubham-96k <iamcoder8989@gmail.com>
2024-10-29 00:45:24 +0530

    commit changes scroll to top

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -9,9 +10,22 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export class FooterComponent implements OnInit {
   faLinkedin = faLinkedin
   faGithub = faGithub
+  faArrowUp = faArrowUp
+  isButtonVisible = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    this.isButtonVisible = scrollPosition > 1000;
+  }
 
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
